f31cd589e36ddbe6ba793bd94636b4885f6eebad
sidd19898 <anganesiddhu@gmail.com>
2026-05-01 00:39:31 +0530

    fix database connection

--- Backend/index.js
+++ Backend/index.js
@@ -139,6 +139,6 @@ else if (req.method === "DELETE" && req.url.startsWith("/delete/")) {
 
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+server.listen(5000,"0.0.0.0", () => {
+  console.log("Server running at http://localhost:5000");
 });
