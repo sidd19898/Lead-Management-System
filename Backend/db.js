17bbb9a7dc47133051c0a3826c11bcdee9a8dfc3
sidd19898 <anganesiddhu@gmail.com>
2026-05-01 00:37:37 +0530

    minor changes

--- Backend/db.js
+++ Backend/db.js
@@ -2,11 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  database: "LMS",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 async function testDB() {
